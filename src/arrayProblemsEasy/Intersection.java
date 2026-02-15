c3fe706b5f96bec0d38a8f3226f3995507f34d14
M.S. MOHAMED MAHSHOQ <msmahshoq2005@gmail.com>
2026-02-15 09:54:01 +0530

    missing done 4 approaches

---
+++ src/arrayProblemsEasy/Intersection.java
@@ -0,0 +1,75 @@
+package arrayProblemsEasy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class Intersection {
+    public static void main(String[] args) {
+        int[]arr1 = {1,2,2,3,4};
+        int[]arr2 = {2,2,2,3};
+//        int[] result = bruteForce(arr1, arr2);
+//        System.out.println(Arrays.toString(result));
+         List<Integer> result = twoPointer(arr1, arr2);
+         Iterator it = result.iterator();
+         while(it.hasNext()){
+             System.out.print(it.next()+" ");}
+
+    }
+    //Time complexity O(n + m) where n and m are the lengths of the two arrays
+    //Space complexity O(n) where n is the length of the smaller array
+    private static List<Integer> twoPointer(int[] arr1, int[] arr2) {
+        int i=0;
+        int j=0;
+        List<Integer> list = new ArrayList<>();
+        while(i<arr1.length && j<arr2.length){
+            if(arr1[i]==arr2[j]) {
+                list.add(arr1[i]);
+                i++;
+                j++;
+            }
+            else if (arr1[i]<arr2[i]){
+                i++;
+            }
+            else{
+                j++;
+            }
+
+
+        }
+
+        return list;
+
+    }
+
+    //Time complexity O(n*m) where n and m are the lengths of the two arrays
+    //Space complexity O(n) where n is the length of the smaller array
+    public static int[] bruteForce(int[] arr1, int[] arr2){
+        int[] visited = new int[arr2.length];
+        List<Integer> list = new ArrayList<>();
+
+
+        for(int i=0;i<arr1.length;i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                if (arr1[i] == arr2[j] && visited[j]==0) {
+                    list.add(arr1[i]);
+                    visited[j] = 1;
+                    break;
+                }
+                if(arr2[j]>arr1[i]) break;
+            }
+        }
+        int[] ans=new int[list.size()];
+        int i=0;
+        for(int n : list){
+            ans[i] = n;
+            i++;
+        }
+
+        return ans;
+
+    }
+
+
+}
---
+++ src/arrayProblemsEasy/Missing.java
@@ -0,0 +1,68 @@
+package arrayProblemsEasy;
+
+public class Missing {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,5};
+        bruteForce(arr);
+        better(arr);
+        optimal(arr);
+        betterOptimal(arr);
+    }
+
+    private static void betterOptimal(int[] arr) {
+        int xor=0;
+        int xor2=0;
+        int n=arr.length+1;
+        for(int i=0;i<arr.length;i++){
+            xor^=arr[i];
+            xor2^=i+1;
+        }
+        xor2=xor2^n;
+        System.out.println(xor^xor2);
+    }
+
+    private static void optimal(int[] arr) {
+        int n=arr.length+1;
+        int total=n*(n+1)/2;
+        int sum=0;
+
+        for(int i=0;i<arr.length;i++){{
+            sum+=arr[i];
+        }}
+
+        System.out.println(total-sum);
+    }
+
+    private static void better(int[] arr) {
+        int n=arr.length+1;
+        int[] hash = new int[n+1];
+        for(int i=0;i<n-1;i++){
+            hash[arr[i]]++;
+        }
+        for(int i=1;i<=n;i++){
+            if(hash[i]==0){
+                {
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void bruteForce(int[] arr) {
+        for(int i=1;i<= arr.length;i++){
+            int flag=0;
+            for(int j=0;j< arr.length;j++){
+                if(arr[j]==i){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0){
+                System.out.println(i);
+                break;
+            }
+        }
+
+    }
+}
